65337f7699ef53724e5d0f22ef81cf6fa8f0d05c
Guilherme Peruci <guilherme.f.peruci@gmail.com>
2023-11-26 23:34:52 -0300

    vwr

--- Gerenciadores/Gerenciador_Estados.h
+++ Gerenciadores/Gerenciador_Estados.h
@@ -30,8 +30,6 @@ namespace Gerenciadores
         static Gerenciador_Estados* get_instancia();
         void setEstadoAtual (const int i);
         const int getEstadoAtual();
-        void setEstadoAtual (int i);
-        int getEstadoAtual();
         int getEstadoAnterior();
         void pausePMenu();
         void addEstado (Estados::Estado* pEstado);
